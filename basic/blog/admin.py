d7626f85bcdc81ac25ba5d369aa5c851685de667
Joeri Djojosoeparto <jsoejitno@gmail.com>
2010-07-26 09:45:29 +0200

    Changed admin wmdeditor to markitup

--- basic/blog/admin.py
+++ basic/blog/admin.py
@@ -1,19 +1,22 @@
 from django.contrib import admin
 from basic.blog.models import *
-from admin_wmdeditor import WmdEditorModelAdmin
+from markitup.widgets import AdminMarkItUpWidget
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 admin.site.register(Category, CategoryAdmin)
 
-class PostAdmin(WmdEditorModelAdmin):
+class PostAdmin(admin.ModelAdmin):
     list_display  = ('title', 'publish', 'status')
     list_filter   = ('publish', 'categories', 'status')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
-    wmdeditor_fields = ('tease','body')
-admin.site.register(Post, PostAdmin)
 
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name in ['body', 'tease']:
+            kwargs['widget'] = AdminMarkItUpWidget()
+        return super(PostAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+admin.site.register(Post, PostAdmin)
 
 class BlogRollAdmin(admin.ModelAdmin):
     list_display = ('name', 'url', 'sort_order',)
